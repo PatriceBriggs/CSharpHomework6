f17dde2c0edae8242743c681f69b8c37f24271cc
Patrice Briggs <PBriggs@perkinscoie.com>
2019-05-23 11:46:27 -0700

    INitial commit HOmework 6

---
+++ BirthdayCardGenerator/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using BirthdayCardGenerator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BirthdayCardGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(BirthdayCard birthdayCard )
+        {
+
+            if (ModelState.IsValid)
+            {
+
+                return View("CardSent", birthdayCard);
+            }
+            else
+            {
+                return View("Index", birthdayCard);
+            }
+        }
+    }
+}
---
+++ BirthdayCardGenerator/Models/BirthdayCard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BirthdayCardGenerator.Models
+{
+    public class BirthdayCard
+    {
+        [Required(ErrorMessage = "Please enter To")]
+        [Display(Name = "To: ")]
+        public string To { get; set; }
+
+        [Required(ErrorMessage = "Please enter From")]
+        [Display(Name = "From: ")]
+        public string From { get; set; }
+
+        [Required(ErrorMessage = "Please enter Message")]
+        [Display(Name = "Message: ")]
+        public string Message { get; set; }
+    }
+}
